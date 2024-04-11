643b7c1c51266186d5e5053a16edb59b93e3322c
kasun <88395585+ka0un@users.noreply.github.com>
2024-04-12 03:26:31 +0530

    changed session time to 1hr added some comment decorations to config.php

--- config.php
+++ config.php
@@ -1,14 +1,24 @@
 <?php
+//===================================================================
+//██╗     ███╗   ███╗███████╗  Library Management System
+//██║     ████╗ ████║██╔════╝  Version : 1.0 (2024)
+//██║     ██╔████╔██║███████╗  Source : https://lms1.hapangama.com
+//██║     ██║╚██╔╝██║╚════██║  License : MIT
+//███████╗██║ ╚═╝ ██║███████║  Doc : https://docs-lms1.hapangama.com
+//╚══════╝╚═╝     ╚═╝╚══════╝  Contact : kasun@hapangama.com
+//===================================================================
+//SQL Database configuration - Tested on MySQL and MariaDB
+//===================================================================
+const DB_HOST = "129.146.1.186"; /* Your Database Host (without port) */
+const DB_PORT = 3306; /* Your Database Port (usually 3306) */
+const DB_USERNAME = "u14_Rf6AAvsnO7"; /* Your Database Username */
+const DB_PASSWORD = "5!csXM+5^fke.XNpM=fIfI99"; /* Your Database Password */
+const DB_DATABASE = "s14_main"; /* Your Database Name */
 
-//Database configuration
-const DB_HOST = "129.146.1.186";
-const DB_PORT = 3306;
-const DB_USERNAME = "u14_Rf6AAvsnO7";
-const DB_PASSWORD = "5!csXM+5^fke.XNpM=fIfI99";
-const DB_DATABASE = "s14_main";
-
+//===================================================================
 //Session configuration
-const SESSION_LIFETIME_SECONDS = 30;
+//===================================================================
+const SESSION_LIFETIME_SECONDS = 3600; /* 1 hour = 3600 seconds, 1 day = 86400 seconds */
 
-
+//===================================================================
 ?>
