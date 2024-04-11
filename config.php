ce8f93fb33624975a998566d8e46b96c4c31c7a9
kasun <88395585+ka0un@users.noreply.github.com>
2024-04-12 03:07:19 +0530

    removed Deprecated  Database configuration

--- config.php
+++ config.php
@@ -11,11 +11,4 @@ const DB_DATABASE = "s14_main";
 const SESSION_LIFETIME_SECONDS = 30;
 
 
-// Deprecated  Database configuration
-$host = "129.146.1.186";
-$port = 3306;
-$username = "u14_Rf6AAvsnO7";
-$password = "5!csXM+5^fke.XNpM=fIfI99";
-$database = "s14_main";
-
 ?>
