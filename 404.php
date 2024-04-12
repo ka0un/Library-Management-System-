f2aa949dcb208d04495e1453f33b71eadf524505
Your Name <you@example.com>
2024-04-12 13:41:38 +0530

    404page

---
+++ 404.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<link rel="stylesheet" href="404.css">
+</head>
+<body>
+	<br><br><br><br>
+	<h1>404</h1>
+	<p>Lost the labyrinth of books.Seek elsewhere, reader.</p>
+	<a href=""><p>Back To Home</p></a>
+</body>
+</html>
