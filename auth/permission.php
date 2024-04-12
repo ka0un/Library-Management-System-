656f9390e1767d9b4fba8226c6b573e50b98b0f6
kasun <88395585+ka0un@users.noreply.github.com>
2024-04-12 10:10:40 +0530

    fixed permission manager variable issues

--- auth/permission.php
+++ auth/permission.php
@@ -4,14 +4,12 @@ require_once __DIR__ . '/../config.php';
 
 function has_permission($userId, $permission): bool
 {
-    global $PERMISSIONS;
     $roleId = get_role($userId);
-    return in_array($permission, $PERMISSIONS[$roleId]);
+    return in_array($permission, PERMISSIONS[$roleId]);
 }
 
 function get_role($userId) {
-    global $STAFF;
-    foreach ($STAFF as $roleId => $users) {
+    foreach (STAFF as $roleId => $users) {
         if (in_array($userId, $users)) {
             return $roleId;
         }
@@ -19,13 +17,12 @@ function get_role($userId) {
     return get_role_name(DEFAULT_ROLE);
 }
 
-function get_role_name($roleId) {
-    global $ROLES;
-    return $ROLES[$roleId] ?? null;
+function get_role_name($roleId): ?string
+{
+    return ROLES[$roleId] ?? null;
 }
 
 function get_role_id($roleName) {
-    global $ROLES;
-    return array_search($roleName, $ROLES);
+    return array_search($roleName, ROLES);
 }
 
