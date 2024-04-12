565eaf5f45d7df4b41c493e6b8ed0ecfe46efb2a
Your Name <you@example.com>
2024-04-12 14:26:17 +0530

    Naduli

---
+++ contact_us.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<link rel="stylesheet" href="contactUs.css">
+</head>
+<body>
+	<div class="paddingclass">
+	<div class="bordercolor">
+	<form action="" method="POST" class="formclass">
+		<br>
+		<input id="textbox" type="text" name="name" class="name" placeholder="Name"><br><br>
+		<input id="textbox" type="email" name="email" class="email" placeholder="Email"><br><br>
+		<textarea name="textarea" class="textarea" rows="5" cols="24">content</textarea><br><br>
+		<button class="sendmsg"><img width="20" height="17" src="https://img.icons8.com/plasticine/100/speech-bubble.png" alt="speech-bubble"/> Send Message</button><br>
+		<p><img width="20" height="17" src="https://img.icons8.com/windows/32/phone.png" alt="phone"/> Assistance: 011 259211</p>
+		<p>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<img width="20" height="17" src="https://img.icons8.com/cotton/64/secured-letter--v3.png" alt="secured-letter--v3"/> Email: Kasun@hapangama.com</p>
+		<p><img width="20" height="17" src="https://img.icons8.com/ios/50/whatsapp--v1.png" alt="whatsapp--v1"/> Whatsapp: 077 112 9081</p>
+	</form>
+	</div>
+	</div>
+</body>
+</html>
