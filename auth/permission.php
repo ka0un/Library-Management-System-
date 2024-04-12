8b30dfc1d064df1277952d15445a0977a258d1af
kasun <88395585+ka0un@users.noreply.github.com>
2024-04-12 11:10:44 +0530

    fixed small issues with permission.php and debug info of index.php

--- auth/permission.php
+++ auth/permission.php
@@ -5,19 +5,20 @@ require_once __DIR__ . '/../config.php';
 function has_permission($userId, $permission): bool
 {
     $roleId = get_role($userId);
-    if (isset($roleId) && isset(PERMISSIONS[$roleId])) {
+    if (isset(PERMISSIONS[$roleId])) {
         return in_array($permission, PERMISSIONS[$roleId]);
     }
     return false;
 }
 
-function get_role($userId) {
+function get_role($userId) : int
+{
     foreach (STAFF as $roleId => $users) {
         if (in_array($userId, $users)) {
             return $roleId;
         }
     }
-    return DEFAULT_ROLE !== null ? get_role_name(DEFAULT_ROLE) : null;
+    return DEFAULT_ROLE;
 }
 
 function get_role_name($roleId): ?string{
--- index.php
+++ index.php
@@ -27,13 +27,13 @@ require_once __DIR__ . '/auth/permission.php';
         <li>Your Token : <?php echo $_SESSION["token"]; ?></li>
         <li>Token Time Left : <?php echo get_session_time_left ($_SESSION["token"]); ?></li>
     </ul>
-    <a href="logout.php">Logout</a>
     <br>
     <h2>Debug Permission Info</h2>
     <ul>
-        <li>Role : <?php echo get_role($_SESSION["uuid"]); ?></li>
-        <li>Role ID : <?php echo get_role_id($_SESSION[get_role($_SESSION["uuid"])]); ?></li>
-        <li>Has Admin Permission : <?php echo has_permission($_SESSION["uuid"], "VIEW_ADMIN_DASHBOARD"); ?></li>
+        <li>Role : <?php echo get_role_name(get_role($_SESSION["uuid"])); ?></li>
+        <li>Role ID : <?php echo get_role($_SESSION["uuid"]); ?></li>
+        <li>Has Admin Permission : <?php if (has_permission($_SESSION["uuid"], "VIEW_ADMIN_DASHBOARD")) echo "true"; else echo "false" ?></li>
+        <li>Has Every Permission : <?php if (has_permission($_SESSION["uuid"], "RADMOM")) echo "true"; else echo "false" ?></li>
     </ul>
     <br>
     <a href="logout.php">Logout</a>
