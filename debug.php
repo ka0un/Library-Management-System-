6954480d391fbf3224c14c89fe7e201e0d11504b
kasun <88395585+ka0un@users.noreply.github.com>
2024-04-12 14:32:06 +0530

    moved debug info to seperate debug panel

---
+++ debug.php
@@ -0,0 +1,41 @@
+<?php
+include(__DIR__ . '/auth/session.php');
+require_once __DIR__ . '/sql/users.php';
+require_once __DIR__ . '/sql/token.php';
+require_once __DIR__ . '/auth/permission.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Debug</title>
+</head>
+<body>
+<H1>Debug Panel</H1>
+<br>
+<h2>Debug User Info</h2>
+<ul>
+    <li>Hi, <?php echo get_user_name($_SESSION["uuid"]); ?></li>
+    <li>Your Email : <?php echo get_user_email($_SESSION["uuid"]); ?></li>
+    <li>Your NIC : <?php echo get_user_nic($_SESSION["uuid"]); ?></li>
+</ul>
+<br>
+<h2>Debug Token Info</h2>
+<ul>
+    <li>Your UUID : <?php echo $_SESSION["uuid"]; ?></li>
+    <li>Your Token : <?php echo $_SESSION["token"]; ?></li>
+    <li>Token Time Left : <?php echo get_session_time_left ($_SESSION["token"]); ?></li>
+</ul>
+<br>
+<h2>Debug Permission Info</h2>
+<ul>
+    <li>Role : <?php echo get_role_name(get_role($_SESSION["uuid"])); ?></li>
+    <li>Role ID : <?php echo get_role($_SESSION["uuid"]); ?></li>
+    <li>Has Admin Permission : <?php if (has_permission($_SESSION["uuid"], "VIEW_ADMIN_DASHBOARD")) echo "true"; else echo "false" ?></li>
+    <li>Has Every Permission : <?php if (has_permission($_SESSION["uuid"], "RADMOM")) echo "true"; else echo "false" ?></li>
+</ul>
+<br>
+<a href="index.php">Back to home</a>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 include(__DIR__ . '/auth/session.php');
-require_once __DIR__ . '/sql/users.php';
-require_once __DIR__ . '/sql/token.php';
-require_once __DIR__ . '/auth/permission.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,27 +11,7 @@ require_once __DIR__ . '/auth/permission.php';
 <body>
     <H1>Welcome To Homepage</H1>
     <br>
-    <h2>Debug User Info</h2>
-    <ul>
-        <li>Hi, <?php echo get_user_name($_SESSION["uuid"]); ?></li>
-        <li>Your Email : <?php echo get_user_email($_SESSION["uuid"]); ?></li>
-        <li>Your NIC : <?php echo get_user_nic($_SESSION["uuid"]); ?></li>
-    </ul>
-    <br>
-    <h2>Debug Token Info</h2>
-    <ul>
-        <li>Your UUID : <?php echo $_SESSION["uuid"]; ?></li>
-        <li>Your Token : <?php echo $_SESSION["token"]; ?></li>
-        <li>Token Time Left : <?php echo get_session_time_left ($_SESSION["token"]); ?></li>
-    </ul>
-    <br>
-    <h2>Debug Permission Info</h2>
-    <ul>
-        <li>Role : <?php echo get_role_name(get_role($_SESSION["uuid"])); ?></li>
-        <li>Role ID : <?php echo get_role($_SESSION["uuid"]); ?></li>
-        <li>Has Admin Permission : <?php if (has_permission($_SESSION["uuid"], "VIEW_ADMIN_DASHBOARD")) echo "true"; else echo "false" ?></li>
-        <li>Has Every Permission : <?php if (has_permission($_SESSION["uuid"], "RADMOM")) echo "true"; else echo "false" ?></li>
-    </ul>
+    <a href="debug.php">Debug</a>
     <br>
     <a href="logout.php">Logout</a>
 </body>
