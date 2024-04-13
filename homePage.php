e7be678f52044b03951cedea2095e6346e556e66
DaniruDodangoda <danirudevanmith2003@gmail.com>
2024-04-13 10:10:29 +0530

    Daniru

---
+++ homePage.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" charset="UTF-8">
+    <link rel="stylesheet" href="styles.css" type="text/css">
+  </head>
+  <body>
+    <div class="slideshow-container">
+      <div class="mySlides fade">
+        <div class="numbertext">1 / 3</div>
+        <img src="images/image1.jpg" style="width:100%">
+        <div class="text">Caption Text</div>
+      </div>
+      <div class="mySlides fade">
+        <div class="numbertext">2 / 3</div>
+        <img src="images/image2.jpg" style="width:100%">
+        <div class="text">Caption Two</div>
+      </div>
+      <div class="mySlides fade">
+        <div class="numbertext">3 / 3</div>
+        <img src="images/image3.jpg" style="width:100%">
+        <div class="text">Caption Three</div>
+      </div>
+    </div>
+    <br>
+    <div style="text-align:center">
+      <span class="dot"></span> 
+      <span class="dot"></span> 
+      <span class="dot"></span> 
+    </div>
+    <br><br>
+    <div class="a">Announcements</div>
+    <div style="max-height:250px;overflow-y:auto">
+      <table border="3px" width="100%">
+        <tr>
+          <th>Date</th>
+          <th>Time</th>
+          <th>Announcement</th>
+        </tr>
+        <tr>
+          <td>10/11</td>
+          <td>08.11</td>
+          <td>
+            <b>Announcement Title</b><br>
+            Short Announcement Description Littebit Longer Longer than you expected exactly this much longer littlebit more
+          </td>
+        </tr>
+        <tr>
+          <td>10/11</td>
+          <td>08.11</td>
+          <td>
+            <b>Announcement Title</b><br>
+            Short Announcement Description Littebit Longer Longer than you expected exactly this much longer littlebit more
+          </td>
+        </tr>
+        <tr>
+          <td>10/11</td>
+          <td>08.11</td>
+          <td>
+            <b>Announcement Title</b><br>
+            Short Announcement Description Littebit Longer Longer than you expected exactly this much longer littlebit more
+          </td>
+        </tr>
+        <tr>
+          <td>10/11</td>
+          <td>08.11</td>
+          <td>
+            <b>Announcement Title</b><br>
+            Short Announcement Description Littebit Longer Longer than you expected exactly this much longer littlebit more
+          </td>
+        </tr>
+        <tr>
+          <td>10/11</td>
+          <td>08.11</td>
+          <td>
+            <b>Announcement Title</b><br>
+            Short Announcement Description Littebit Longer Longer than you expected exactly this much longer littlebit more
+          </td>
+        </tr>
+        <tr>
+          <td>10/11</td>
+          <td>08.11</td>
+          <td>
+            <b>Announcement Title</b><br>
+            Short Announcement Description Littebit Longer Longer than you expected exactly this much longer littlebit more
+          </td>
+        </tr>
+      </table>
+    </div>
+    <br><br>
+    <div class="a">Popular Books</div>
+    <div class="b">
+      <div class="scroll-container">
+        <img src="images/book1.jpeg" height="300px" width="200px"> 
+        <img src="images/book2.jpeg" height="300px" width="200px">
+        <img src="images/book3.jpeg" height="300px" width="200px">
+        <img src="images/book4.jpeg" height="300px" width="200px">
+        <img src="images/book5.jpeg" height="300px" width="200px">
+        <img src="images/book6.jpeg" height="300px" width="200px">
+        <img src="images/book7.jpeg" height="300px" width="200px">
+        <img src="images/book8.jpeg" height="300px" width="200px">
+        <img src="images/book9.jpeg" height="300px" width="200px">
+        <img src="images/book10.jpeg" height="300px" width="200px">
+      </div>
+    </div>
+    <script src="script.js"></script>
+  </body>
+</html>
