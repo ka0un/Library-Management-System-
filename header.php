4dbeed6ad154fe0c10ba1d23e05892a951f9b5b7
kavindugit <IT23249684@my.sliit.lk>
2024-04-13 15:17:14 +0530

    header footer not basic structure

---
+++ header.php
@@ -0,0 +1,69 @@
+<?php
+    function header($pathLinks)
+    {
+        $html = '<!DOCTYPE html>
+            <html lang="en">
+                <head>
+                    <title>LMS</title>
+                    <link rel ="stylesheet" href="header.css">
+                    
+            
+                </head>
+                <body>
+                    
+                        <div class ="part1">
+                        
+                            <div class="imgbook">
+                                <img src ="images/books.svg">
+                            </div>
+                            <div class="brand">
+                                    SLIIT Main Library
+                            </div>
+                            <div class="nav">
+                                <div class ="navhome"><a href ="#">HOME</a></div>
+                                <div class ="navbook"><a href ="#">BOOK</a></div>
+                                <div class ="navdashboard"><a href="#">DASHBOARD</a></div>
+
+                            </div>
+                            <div class="rightcon">
+                                <div class="text">
+                                    <div class ="hellow"> Hellow User!</div>
+                                    <div class ="log"><a href="register/login.php">Login</a></div>
+                                </div>
+                                <div class ="userimg"> <img src ="images/userimage2.jpg"></div>
+                                    
+                            
+            
+                            </div>
+            
+                            
+            
+                        </div>
+            
+                        <div class ="part2">
+                            <div class ="path">
+                                <a href ="#">HOME</a> > ';
+
+
+        foreach($pathLinks as $path)
+        {
+            $html .= '<a href = "'.$path['url']. '" >'.$path['text'].'</a></div';
+        } 
+
+        $html .= '   </div>
+                  </div>
+                  </body>
+                  </html>';
+
+        return $html;
+    }
+
+?>
+
+
+
+<!--example arry------ 
+    $navLinks = array(
+    array('text' => 'Book', 'url' => '#'),
+    array('text' => 'Dashboard', 'url' => '#')
+);  -->
