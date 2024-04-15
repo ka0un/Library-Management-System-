fbf2ed14d8282b54aace0f37781360f396769253
kavindugit <IT23249684@my.sliit.lk>
2024-04-15 22:44:46 +0530

    bookpage-sidebar , admin -side bar

---
+++ components/admin-sideBar.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lng="en">
+    <head>
+        
+    </head>
+    <body>
+        <style>
+            .sidebar{
+                background-color: #292929;
+                border:3px;
+                border-radius: 20px;
+                width:18%;
+                height:72vh;
+                overflow-y: auto;
+
+            }
+
+            .parts{
+                background-color: #D9D9D9;
+                border:3px;
+                border-radius: 20px;
+                width:auto;
+                height:auto;
+                overflow-y: auto;
+                margin: 10px;
+                padding:6px;
+                
+            }
+
+            .text{
+                height:40px;
+                border: 2px solid black;
+                margin:5px;
+                padding:2px;
+                background-color: whitesmoke;
+                justify-content: center;
+                display: flex;
+                align-items: center; 
+                justify-content: center; 
+            }
+        </style>
+        <div class="sidebar">
+            <div class="parts">
+                
+                <div class="text">Add Book</div>
+                <div class="text">Remove Book</div>
+                <div class="text">Update Book</div>
+          
+
+         
+            </div>
+            <br>
+            <div class="parts">
+                <div class="text">Update User</div>
+                <div class="text">Ban User</div>
+            </div>
+        </div>
+    </body>
+</html>
