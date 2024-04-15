d1513c48552a67f157293fc5d0c9d84516b199f0
Your Name <you@example.com>
2024-04-15 21:48:14 +0530

    contact_us

---
+++ contact_us.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<link rel="stylesheet" href="contact_us.css">
+</head>
+<body>
+	<div class="paddingclass">
+	<div class="bordercolor">
+	<form action="" method="POST" class="formclass">
+		<br>
+		<input id="textbox" type="text" name="name" class="name" placeholder="Enter Name"><br><br>
+		<input id="textbox" type="email" name="email" class="email" placeholder="Enter Email"><br><br>
+		<textarea id="textarea" name="textarea" class="textarea" rows="5" cols="23">content</textarea><br><br>
+		<button class="sendmsg"><img width="20" height="17" src="https://img.icons8.com/glyph-neue/64/1A1A1A/sent.png" alt="sent"/> Send Message</button><br>
+		<p><img width="20" height="17" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/ringer-volume.png" alt="ringer-volume"/> &nbsp Assistance: 011 259211</p>
+		<p>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<img width="20" height="17" src="https://img.icons8.com/cotton/64/secured-letter--v3.png" alt="secured-letter--v3"/> Email: Kasun@hapangama.com</p>
+		<p><img width="20" height="17" src="https://img.icons8.com/ios-filled/50/FFFFFF/whatsapp--v1.png" alt="whatsapp--v1"/> Whatsapp: 077 112 9081</p>
+	</form>
+	</div>
+	</div>
+</body>
+</html>
