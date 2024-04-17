7245b0f2df2697990d365099ee27442f1036401a
DaniruDodangoda <danirudevanmith2003@gmail.com>
2024-04-17 14:33:16 +0530

    Added user dashboard

---
+++ userDashboard.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" charset="UTF-8">
+        <link rel="stylesheet" href="userdashboard.css" type="text/css">
+    </head>
+    <body>
+        <div class="sidebar">
+            <br><br><img src="Images/img_avatar.png" width="149px" height="150px" alt="Profile Picture" style="border-radius:50%;border:3px solid #D9D9D9">
+            <h2>Full Name</h2>
+            <div class="sidebartext">
+                Role
+            </div><br>
+            <div class="sidebarul">
+                <ul>
+                    <li>Total Books Reserved 22</li>
+                    <li>Total Books Returned 9</li>
+                    <li>Total Fine Paid 20$</li>
+                </ul>
+            </div><br><br><br><br><br>
+            <button style="border:3px solid #D9D9D9;font-size:22px;background-color:#292929;color:#D9D9D9;border-radius:20px">Change Password</button><br><br>
+            <div class="sidebarbutton2">
+                <button style="border:3px solid #D9D9D9;background-color:#D9D9D9;color:#292929;font-size:22px;border-radius:20px"><b>Logout</b></button>
+            </div><br>
+        </div>
+        <div class="rightside">
+            <div class="box">
+                <div class="box1">
+                    Reserved<br>
+                    03
+                </div>
+                <div class="box2">
+                    Pending<br>
+                    01
+                </div>
+                <div class="box3">
+                    Overdue<br>
+                    01
+                </div>
+                <div class="box4">
+                    Total Fine<br>
+                    10$
+                </div>
+            </div><br>
+            <div class="table">
+                <table border="3px">
+                    <div>
+                        <tr>
+                            <div>
+                                <th>Checkout Date</th>
+                            </div>
+                            <div>
+                                <th>Book</th>
+                            </div>
+                            <div>
+                                <th>Return Date</th>
+                            </div>
+                            <div>
+                                <th>Status</th>
+                            </div>
+                        </tr>
+                    </div>
+                    <div>
+                        <tr>
+                            <div>
+                                <td>2024/03/23</td>
+                            </div>
+                            <div>
+                                <td>Cherry Book</td>
+                            </div>
+                            <div>
+                                <td>2024/03/26</td>
+                            </div>
+                            <div>
+                                <td>Pending</td>
+                            </div>
+                        </tr>
+                    </div>
+                    <div>
+                        <tr>
+                            <div>
+                                <td>2024/03/22</td>
+                            </div>
+                            <div>
+                                <td>Online Book</td>
+                            </div>
+                            <div>
+                                <td>-</td>
+                            </div>
+                            <div>
+                                <td>Reserved</td>
+                            </div>
+                        </tr>
+                    </div>
+                    <div>
+                        <tr>
+                            <div>
+                                <td>2024/03/21</td>
+                            </div>
+                            <div>
+                                <td>Kapila Book</td>
+                            </div>
+                            <div>
+                                <td>-</td>
+                            </div>
+                            <div>
+                                <td>Reserved</td>
+                            </div>
+                        </tr>
+                    </div>
+                    <div>
+                        <tr>
+                            <div>
+                                <td>2024/03/20</td>
+                            </div>
+                            <div>
+                                <td>Koritha Book</td>
+                            </div>
+                            <div>
+                                <td>2024/03/22</td>
+                            </div>
+                            <div>
+                                <td>Overdue (10$)</td>
+                            </div>
+                        </tr>
+                    </div>
+                    <div>
+                        <tr>
+                            <div>
+                                <td>2024/03/20</td>
+                            </div>
+                            <div>
+                                <td>Chalani Book</td>
+                            </div>
+                            <div>
+                                <td>-</td>
+                            </div>
+                            <div>
+                                <td>Reserved</td>
+                            </div>
+                        </tr>
+                    </div>
+                    <div>
+                        <tr>
+                            <div>
+                                <td>2024/03/26</td>
+                            </div>
+                            <div>
+                                <td>The Book</td>
+                            </div>
+                            <div>
+                                <td>2024/03/25</td>
+                            </div>
+                            <div>
+                                <td>Returned</td>
+                            </div>
+                        </tr>
+                    </div>
+                    <div>
+                        <tr>
+                            <div>
+                                <td>2024/01/26</td>
+                            </div>
+                            <div>
+                                <td>Random Book</td>
+                            </div>
+                            <div>
+                                <td>2024/02/25</td>
+                            </div>
+                            <div>
+                                <td>Returned</td>
+                            </div>
+                        </tr>
+                    </div>
+                    <div>
+                        <tr>
+                            <div>
+                                <td></td>
+                            </div>
+                            <div>
+                                <td></td>
+                            </div>
+                            <div>
+                                <td></td>
+                            </div>
+                            <div>
+                                <td></td>
+                            </div>
+                        </tr>
+                    </div>
+                    <div>
+                        <tr>
+                            <div>
+                                <td></td>
+                            </div>
+                            <div>
+                                <td></td>
+                            </div>
+                            <div>
+                                <td></td>
+                            </div>
+                            <div>
+                                <td></td>
+                            </div>
+                        </tr>
+                    </div>
+                    <div>
+                        <tr>
+                            <div>
+                                <td></td>
+                            </div>
+                            <div>
+                                <td></td>
+                            </div>
+                            <div>
+                                <td></td>
+                            </div>
+                            <div>
+                                <td></td>
+                            </div>
+                        </tr>
+                    </div>
+                    <div>
+                        <tr>
+                            <div>
+                                <td></td>
+                            </div>
+                            <div>
+                                <td></td>
+                            </div>
+                            <div>
+                                <td></td>
+                            </div>
+                            <div>
+                                <td></td>
+                            </div>
+                        </tr>
+                    </div>
+                    <div>
+                        <tr>
+                            <div>
+                                <td></td>
+                            </div>
+                            <div>
+                                <td></td>
+                            </div>
+                            <div>
+                                <td></td>
+                            </div>
+                            <div>
+                                <td></td>
+                            </div>
+                        </tr>
+                    </div>
+                </table>
+            </div>
+        </div>
+    </body>
+</html>
